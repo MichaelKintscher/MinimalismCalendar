87a74b1e981149e0da8a82cf8134c837f109b503
Michael Kintscher <michael@kintscher.dev>
2021-06-06 00:18:29 -0700

    Added DateUtility and a height property for agenda times on CalendarControl.

--- MinimalismCalendar/UserControls/CalendarControl.xaml.cs
+++ MinimalismCalendar/UserControls/CalendarControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -17,11 +18,45 @@ using Windows.UI.Xaml.Navigation;
 
 namespace MinimalismCalendar.UserControls
 {
-    public sealed partial class CalendarControl : UserControl
+    public sealed partial class CalendarControl : UserControl, INotifyPropertyChanged
     {
+        #region Properties
+        private double agendaTimeUnitHeight;
+        /// <summary>
+        /// Height of each vertical unit of time on the agenda view.
+        /// </summary>
+        public double AgendaTimeUnitHeight
+        {
+            get => this.agendaTimeUnitHeight;
+            set
+            {
+                this.agendaTimeUnitHeight = value;
+                this.RaisePropertyChanged("TimeUnitHeight");
+            }
+        }
+        #endregion
+
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// Raise the PropertChanged event for the given property name.
+        /// </summary>
+        /// <param name="name">Name of the property changed.</param>
+        public void RaisePropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        #endregion
+
         public CalendarControl()
         {
             this.InitializeComponent();
+
+            // Initialize the height of the agenda time units.
+            this.AgendaTimeUnitHeight = 50;
         }
     }
 }
---
+++ MinimalismCalendar/Utility/DateUtility.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimalismCalendar.Utility
+{
+    /// <summary>
+    /// Contains methods for date calculations.
+    /// </summary>
+    public static class DateUtility
+    {
+        /// <summary>
+        /// Gets the date of the Sunday preceeding the given date.
+        /// </summary>
+        /// <param name="date">The date to find the Sunday prior to.</param>
+        /// <returns>The date of the Sunday preceding the given date.</returns>
+        public static DateTime GetPreviousSunday(DateTime date)
+        {
+            return date.AddDays(-(int)date.DayOfWeek);
+        }
+
+        /// <summary>
+        /// Gets the date of the Saturday following the given date.
+        /// </summary>
+        /// <param name="date">The date to find the Saturday following.</param>
+        /// <returns>The date of the Saturday following the given date.</returns>
+        public static DateTime GetNextSaturday(DateTime date)
+        {
+            return date.AddDays(6 - (int)date.DayOfWeek);
+        }
+    }
+}
